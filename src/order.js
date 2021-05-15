f6d8249ea4f55d1a47cb1b47ed96d2fd56a92177
Hayley Dixon <hdixon37@gmail.com>
2021-05-15 17:45:50 -0600

    Finilized Order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(order, orderTypes) {
+  if (orderTypes.length < 3) {
+      orderTypes.push(order)
+      return orderTypes;
+  }
+};
+
+function refundOrder(orderNumRefund, orderTypes) {
+  for (i = 0; i < orderTypes.length; i++) {
+    if (orderTypes[i].orderNumber === orderNumRefund) {
+      orderTypes.splice(i, 1)
+    }
+  } return orderTypes; // returns the orderType with order removed
+};
+
+function listItems(orderTypes) {
+  var list = [];
+    for (i = 0; i < orderTypes.length; i++) {
+    list.push(orderTypes[i].item);
+  }
+  var printList = list.join(", ");
+  return printList;
+};
+
+function searchOrder(orderTypes, itemSearch) {
+  var list = [];
+  for (i = 0; i < orderTypes.length; i++) {
+    list.push(orderTypes[i].item);
+  } return list.includes(itemSearch);
+};
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
